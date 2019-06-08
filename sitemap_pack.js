275832b6bfc929ec018abe10f4ca9840b360d40a
clooooode <jackey8616@gmail.com>
2019-06-08 16:42:38 +0800

    Update a sitemap_pack.js to auto-generate
    Remove sitemap.xml to let script auto-generate when build.
    Fix some error og:type and og:url at Novel and Comic.

---
+++ sitemap_pack.js
@@ -0,0 +1,64 @@
+const https =require('https');
+const xmlBuilder = require('xmlbuilder');
+const fs = require('fs');
+
+const frontend = 'https://silverfish.cc/#/';
+const backend = 'https://silverfish-backend.clo5de.info:2087';
+
+let doc = xmlBuilder
+  .create('urlset', { version: '1.0', encoding: 'UTF-8' })
+    .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
+    .att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+    .att('xsi:schemaLocation', 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd')
+    .ele('url')
+      .ele('loc', 'https://silverfish.cc/')
+      .up()
+    .up();
+
+function sync_get(url) {
+  return new Promise((resolve, reject) => {
+    https.get(`${backend}${url}`, (res) => {
+        let allData = '';
+        res.on('data', (data) => {
+          allData += data;
+        });
+      
+        res.on('end', () => {
+            resolve(JSON.parse(allData));
+        })
+      })
+  })
+} 
+
+
+sync_get('/api/v1/novels').then((datas) => {
+  for(every in datas['data']) {
+    doc = doc
+      .ele('url')
+        .ele('loc', `${frontend}novel_intro/${datas['data'][every]['novelID']}`)
+        .up()
+      .up();
+  }
+  return sync_get('/api/v1/comics')
+}).then((datas) => {
+  for(every in datas['data']) {
+    doc = doc
+      .ele('url')
+        .ele('loc', `${frontend}comic_intro/${datas['data'][every]['comicID']}`)
+        .up()
+      .up();
+  }
+  doc = doc.end({ pretty: true });
+  return doc;
+}).then((doc) => {
+  if (fs.existsSync('./public/sitemap.xml')) {
+    fs.renameSync('./public/sitemap.xml', './public/sitemap.xml.bak', (err) => {
+      if (err) throw err;
+        console.log('Rename complete.');
+    });
+  }
+  fs.writeFileSync('./public/sitemap.xml', doc.toString(), (err) => {
+    if (err) throw err;
+  });
+  console.log('Sitemap.xml Generated.')
+})
