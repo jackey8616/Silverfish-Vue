17c006ac12c47ef8528d52393cf49343f807e5f4
clooooode <jackey8616@gmail.com>
2019-10-18 21:14:36 +0800

    Fix img size is wrong

--- src/App.vue
+++ src/App.vue
@@ -43,4 +43,9 @@
   input.form-control::placeholder {
     color: #203744;
   }
+  /* Need to remove after Home component not use as List. */
+  img {
+    width: 100%;
+    height: 100%;
+  }
 </style>
