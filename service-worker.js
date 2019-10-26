17a54debba7e90be0408d33477cb571defd1e709
Clooooode <jackey8616@gmail.com>
2019-10-26 09:39:42 +0000

    Travis Build Bot Update

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+importScripts("/precache-manifest.c050dc135cf3b9709c4c7e3756efb15a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.setConfig({ debug: true });
+
+workbox.clientsClaim();
+workbox.skipWaiting();
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.html'),
+  workbox.strategies.networkFirst({
+    cacheName: 'workbox:html'
+  })
+);
+
+workbox.routing.registerRoute(
+  /.*\.css/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'workbox:css',
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  workbox.strategies.networkFirst({
+    cacheName: 'workbox:js'
+  })
+);
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
