84a4eb030816050e2cd5e5072b8eae357b2425ac
jackey8616 <jackey8616@users.noreply.github.com>
2019-10-26 10:27:10 +0000

    Automated deployment: Sat Oct 26 10:27:10 UTC 2019 62d05357af1bf17282c70788385f5363f465c45d

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.c050dc135cf3b9709c4c7e3756efb15a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.83694c6d5d6fbbc2ae0ddb36aea572d9.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
