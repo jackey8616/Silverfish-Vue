2c0857cb76d081e9cc7132b7a78e90a4a6e25042
jackey8616 <jackey8616@users.noreply.github.com>
2021-01-08 04:40:57 +0000

    Deploying to gh-pages from  @ 36fb762a56aab94e30f3c80a579b4dfab376980f 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.c6bab0681b23c199df483de04fe3f40f.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.78037f1c9433e829527c0df427748be3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
